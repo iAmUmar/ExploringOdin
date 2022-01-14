ed0a2b36eebb5b1322ca916205b64e2993febb8a
Muhammad Umar <umarbinnazir@gmail.com>
2022-01-14 20:20:45 +0500

    Committing after experimenting OdinInspector
    * Just basic setup

---
+++ Assets/Scripts/CustomInspector/ItemSO.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu()]
+public class ItemSO : ScriptableObject
+{
+    [PreviewField(60), HideLabel]
+    [HorizontalGroup("Split", 60)]
+    public Sprite itemSprite;
+
+    [VerticalGroup("Split/Right"), LabelWidth(120)]
+    public string itemName;
+
+    [VerticalGroup("Split/Right"), LabelWidth(120)]
+    public string itemType;
+
+    [VerticalGroup("Split/Right"), LabelWidth(120)]
+    [Range(0, 10)]
+    public int itemAmount;
+
+    [VerticalGroup("Split/Right"), LabelWidth(120)]
+    public int itemPrice;
+}
